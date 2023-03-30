7ab43383eb1416e4cbdee4b28873942e81085df0
UserTripleZero <24bayletri@washk12.org>
2023-03-30 09:22:48 -0600

    Revert "Revised everything"
    This reverts commit bd3b4e66b6a5b62a642fd3ffa774a98690a250f0.

--- Assets/Scripts/CameraControll.cs
+++ Assets/Scripts/CameraControll.cs
@@ -4,32 +4,15 @@ using UnityEngine;
 
 public class CameraControll : MonoBehaviour
 {
-    public Transform player; // reference to the player object
-    public Vector3 offset; // offset the camera from the player
-    public float rotationSpeed; // rotation speed of the camera
-    private float currentVerticalRotation = 0f; // current vertical rotation of the camera
-    private float currentHorizontalRotation = 0f; // current horizontal rotation of the camera
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        // Lock and hide the mouse cursor
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        
     }
-    
-    void LateUpdate()
+
+    // Update is called once per frame
+    void Update()
     {
-        // Update camera position to follow the player
-        transform.position = player.position + offset;
-
-        // Handle mouse input to rotate the camera around the player
-        currentHorizontalRotation += Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
-        currentVerticalRotation -= Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
-
-        // Clamp the vertical rotation so the camera doesn't flip over
-        currentVerticalRotation = Mathf.Clamp(currentVerticalRotation, -89f, 90f);
-
-        transform.rotation = Quaternion.Euler(currentVerticalRotation, currentHorizontalRotation, 0f);
+        t
     }
 }
-
